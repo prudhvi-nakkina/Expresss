cbc0ffbb0e8e634772afe910f286aae958a3ec5a
Prudhvi Nakkina <nakkina.p@northeastern.edu>
2022-04-19 22:10:04 -0400

    Express - right panel - prudhvi

--- express-app/src/components/ConversationComponent.js
+++ express-app/src/components/ConversationComponent.js
@@ -8,8 +8,28 @@ flex: 3;
 background: white;
 `;
 
+const Profileheader = styled.div`
+  display: flex;
+  flex-direction: row;
+  background: #ededed;
+  padding: 10px;
+`;
+
+const ProfileImage = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+`;
+
 const ConversationComponent = () => {
-    return<Container>"ConversationComponent"</Container>;
+    return(
+    <Container>
+        <Profileheader>
+        <ProfileImage src="/profile/elon.jpeg">
+            </ProfileImage>
+        </Profileheader>
+    </Container>
+    );
 }
 
 export default ConversationComponent;
