d7e11da375c6e0d6cd33b2312d67448dc4a9dd00
Prudhvi Nakkina <nakkina.p@northeastern.edu>
2022-04-19 23:18:11 -0400

    Express - create default screen - prudhvi

--- express-app/src/App.js
+++ express-app/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 import ContactListComponent from "./components/ContactListComponent";
 import ConversationComponent from "./components/ConversationComponent";
@@ -10,17 +11,28 @@ const Container = styled.div`
   background: #f8f9fb;
 `;
 
+const Placeholder = styled.div`
+  flex: 3;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  color: rgba(0, 0, 0, 0.45);
+`;
+
+
+
 
 function App() {
+  const [selectedChat, setChat] = useState()
   return (
     <Container>
       <ContactListComponent>
 
       </ContactListComponent>
 
-      <ConversationComponent>
-
-      </ConversationComponent>
+      {selectedChat?<ConversationComponent />:<Placeholder></Placeholder>}
     </Container>
   );
 }
--- express-app/src/mockData.js
+++ express-app/src/mockData.js
@@ -1,28 +1,28 @@
 export const contactList = [
   {
     id: 1,
-    name: "Ayush Kulshrestha",
+    name: "Prudhvi Nakkina",
     profilePic: "/profile/elon.jpeg",
     lastText: "Hey Man",
     lastTextTime: "12:58 PM",
   },
   {
     id: 2,
-    name: "John Appleseed",
+    name: "Akhil Jpeg",
     profilePic: "/profile/jeff.jpeg",
     lastText: `I'm with john`,
     lastTextTime: "12:45 PM",
   },
   {
     id: 3,
-    name: "Elon Bezoz",
+    name: "Charan maila",
     profilePic: "/profile/elon.jpeg",
     lastText: "Where is my Tesla?",
     lastTextTime: "12:30 PM",
   },
   {
     id: 4,
-    name: "Jeff Musk",
+    name: "Sampath Sowri",
     profilePic: "/profile/jeff.jpeg",
     lastText: "Travelling to space",
     lastTextTime: "12:00 PM",
