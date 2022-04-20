d5595af17b8e7754bc3a7cb71a12cc8ae3b59e4e
Sowri19 <98078922+Sowri19@users.noreply.github.com>
2022-04-20 03:45:12 -0400

    express-dbconnect-sowri

--- express-app/server/dbConnection/index.js
+++ express-app/server/dbConnection/index.js
@@ -2,4 +2,13 @@ import mongoose from "mongoose";
 
 const DB_CONNECTION_URL = "mongodb://localhost:27017/test";
 
-const connectDB = () => {};
+const connectDB = () => {
+  const options = {
+    keepAlive: 1,
+    autoReconnect: 1,
+    poolSize: 10,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  return mongoose.connect(DB_CONNECTION_URL, {});
+};
