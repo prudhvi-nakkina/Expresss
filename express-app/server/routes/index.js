1ccacfa94f466f2e356fecfa44fc511a29bb88da
Sowri19 <98078922+Sowri19@users.noreply.github.com>
2022-04-20 05:02:20 -0400

    server-routesnaming-sowri

--- express-app/server/routes/index.js
+++ express-app/server/routes/index.js
@@ -1,13 +1,12 @@
 const applyRoutes = (app) => {
   app.get("/", (req, res) => res.send(`API os running fine`));
   //create-user, login, channel, search-user, channel-list, send-messsage
-  app.post("/", (req, res) => res.send(`API os running fine`));
-  app.post("/", (req, res) => res.send(`API os running fine`));
+  app.post("/user", (req, res) => res.send(`create-user`));
 
-  app.post("/", (req, res) => res.send(`API os running fine`));
-  app.post("/", (req, res) => res.send(`API os running fine`));
-  app.get("/", (req, res) => res.send(`API os running fine`));
-  app.get("/", (req, res) => res.send(`API os running fine`));
+  app.post("/login", (req, res) => res.send(`login`));
+  app.post("/channel", (req, res) => res.send(`channel`));
+  app.get("/search-user", (req, res) => res.send(`search-user`));
+  app.get("/channel-list", (req, res) => res.send(`channel-list`));
   app.post("/message", (req, res) => res.send(`message`));
 };
 
