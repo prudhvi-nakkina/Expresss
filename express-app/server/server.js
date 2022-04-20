677939d5c7c1b250d95f3a81e1be2fcbba3f824e
Sowri19 <98078922+Sowri19@users.noreply.github.com>
2022-04-20 04:36:48 -0400

    server-dbsconnections-sowri

--- express-app/server/server.js
+++ express-app/server/server.js
@@ -1,12 +1,11 @@
 import APP from "express";
 import connectDB from "./dbConnection";
-
 const app = new APP();
 const PORT = 3001;
 const startServer = () => {
   Promise.all([connectDB()]).then(() => {
     app.listen(PORT);
-    console.log(`Server started on port ${PORT}`);
+    console.log(`Server started on Port ${PORT}`);
   });
 };
 
