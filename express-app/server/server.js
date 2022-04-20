1193cd2bbb482b1c3cfc1f70f0f7a1fc4be47c47
Sowri19 <98078922+Sowri19@users.noreply.github.com>
2022-04-20 04:41:19 -0400

    server-config-routes-sowri

--- express-app/server/server.js
+++ express-app/server/server.js
@@ -1,7 +1,7 @@
 import APP from "express";
 import connectDB from "./dbConnection";
 const app = new APP();
-const PORT = 3001;
+const PORT = 3005;
 const startServer = () => {
   Promise.all([connectDB()]).then(() => {
     app.listen(PORT);
