d6cf9dc8e8ff8cdb097a369a973f9b943a8734aa
Sowri19 <98078922+Sowri19@users.noreply.github.com>
2022-04-20 04:47:53 -0400

    server-routes-sowri

---
+++ express-app/server/routes/index.js
@@ -0,0 +1,3 @@
+const applyRoutes = (app) => {};
+
+export default applyRoutes;
--- express-app/server/server.js
+++ express-app/server/server.js
@@ -1,6 +1,8 @@
 import APP from "express";
 import connectDB from "./dbConnection";
+import configureExpressApp from "./config";
 const app = new APP();
+configureExpressApp(app);
 const PORT = 3005;
 const startServer = () => {
   Promise.all([connectDB()]).then(() => {
