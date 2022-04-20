96896ef63890e67c6f04219c8da31c23455f0b4f
Sowri19 <98078922+Sowri19@users.noreply.github.com>
2022-04-20 04:59:34 -0400

    server-routesdefine-sowri

--- express-app/server/routes/index.js
+++ express-app/server/routes/index.js
@@ -1,5 +1,14 @@
 const applyRoutes = (app) => {
   app.get("/", (req, res) => res.send(`API os running fine`));
+  //create-user, login, channel, search-user, channel-list, send-messsage
+  app.post("/", (req, res) => res.send(`API os running fine`));
+  app.post("/", (req, res) => res.send(`API os running fine`));
+
+  app.post("/", (req, res) => res.send(`API os running fine`));
+  app.post("/", (req, res) => res.send(`API os running fine`));
+  app.get("/", (req, res) => res.send(`API os running fine`));
+  app.get("/", (req, res) => res.send(`API os running fine`));
+  app.post("/message", (req, res) => res.send(`message`));
 };
 
 export default applyRoutes;
