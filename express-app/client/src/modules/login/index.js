f60fdb2fe06fee53ad015f2e1aae0650bbc4da9a
Akhil Sai Koppanatham <koppanatham.a@northeastern.edu>
2022-04-21 17:19:53 -0400

    now have to link the chats to each user

--- express-app/client/src/modules/login/index.js
+++ express-app/client/src/modules/login/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { GoogleLogin } from "react-google-login";
 import App from "../../App";
-// import cookieManager from "../../managers/cookieManager";
+import cookieManager from "../../managers/cookieManager";
 
 const Container = styled.div`
   display: flex;
@@ -59,53 +59,57 @@ const QRCode = styled.img`
 `;
 
 const LoginComponent = () => {
-  const [userInfo, setUserInfo] = useState();
+    const [userInfo, setUserInfo] = useState();
 
-//   useEffect(() => {
-//     const userData = cookieManager.getUserInfo();
-//     if (userData) setUserInfo(userData);
-//   }, []);
+    //   useEffect(() => {
+    //     const userData = cookieManager.getUserInfo();
+    //     if (userData) setUserInfo(userData);
+    //   }, []);
 
-//   const responseGoogle = async (response) => {
-//     // await httpManager.createUser({
-//     //   email: response.profileObj.email,
-//     //   name: response.profileObj.name,
-//     //   profilePic: response.profileObj.imageUrl,
-//     // });
-//     setUserInfo(response.profileObj);
-//     cookieManager.setUserInfo(response.profileObj);
-//   };
+      const responseGoogle = async (response) => {
+        // console.log(response);
+        setUserInfo(response.profileObj);
+        console.log(userInfo);
+        // // await httpManager.createUser({
+        // //   email: response.profileObj.email,
+        // //   name: response.profileObj.name,
+        // //   profilePic: response.profileObj.imageUrl,
+        // // });
+        // setUserInfo(response.profileObj);
+        // cookieManager.setUserInfo(response.profileObj);
+        
+      };
 
-  return (
-    <>
-      {userInfo ? (
-        <App userInfo={userInfo} />
-      ) : (
-        <Container>
-          <Header>Expresso Chat</Header>
-          <CardView>
-            <Instructions>
-              <Heading>To use Expresso Chat:</Heading>
-              <ol>
-                <li>You need to Signin using your Google Account.</li>
-                <li>You can anytime logout from the Web.</li>
-                <li>
-                  Click on Signin button to continue using the Expresso.
-                </li>
-              </ol>
-              <GoogleLogin
-                clientId="135907261663-hdilpg79i6h9qg1c0cjg49nv6g7fqdnk.apps.googleusercontent.com"
-                buttonText="Sign In with Google"
-                cookiePolicy={"single_host_origin"}
-                // onSuccess={responseGoogle}
-                // onFailure={responseGoogle}
-              />
-            </Instructions>
-            <QRCode src="whatsapp-clone/qr-placeholder.png" />
-          </CardView>
-        </Container>
-      )}
-    </>
-  );
+    return (
+        <>
+            {userInfo ? (
+                <App userInfo={userInfo} />
+            ) : (
+                <Container>
+                    <Header>Expresso Chat</Header>
+                    <CardView>
+                        <Instructions>
+                            <Heading>To use Expresso Chat:</Heading>
+                            <ol>
+                                <li>You need to Signin using your Google Account.</li>
+                                <li>You can anytime logout from the Web.</li>
+                                <li>
+                                    Click on Signin button to continue using the Expresso.
+                                </li>
+                            </ol>
+                            <GoogleLogin
+                                clientId="135907261663-hdilpg79i6h9qg1c0cjg49nv6g7fqdnk.apps.googleusercontent.com"
+                                buttonText="Sign in with Google"
+                                cookiePolicy={"single_host_origin"}
+                                onSuccess={responseGoogle}
+                                onFailure={responseGoogle}
+                            />
+                        </Instructions>
+                        <QRCode src="https://1000logos.net/wp-content/uploads/2022/01/RANDOM-CHAT-Logo-768x432.png" />
+                    </CardView>
+                </Container>
+            )}
+        </>
+    );
 };
 export default LoginComponent;
