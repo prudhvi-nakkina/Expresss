cfe2b96f886a5bf4fe1a0009c0a31c162c7e32f6
Akhil Sai Koppanatham <koppanatham.a@northeastern.edu>
2022-04-22 01:55:55 -0400

    added send message httpManager

--- express-app/client/src/managers/httpManager.js
+++ express-app/client/src/managers/httpManager.js
@@ -6,7 +6,27 @@ const createUser = async (userData) => {
   return await axios.post(`${API_BASE_URL}/user`, userData);
 };
 
+const searchUser = async (email) => {
+  return await axios.get(`${API_BASE_URL}/search-user?email=${email}`);
+};
+
+const createChannel = async (requestData) => {
+  return await axios.post(`${API_BASE_URL}/channel`, requestData);
+};
+
+const getChannelList = async (email) => {
+  return await axios.get(`${API_BASE_URL}/channel-list?email=${email}`);
+};
+
+const sendMessage = async (requestData) => {
+  return await axios.post(`${API_BASE_URL}/message`, requestData);
+};
+
 export const httpManager = {
-  createUser
+  createUser,
+  searchUser,
+  createChannel,
+  getChannelList,
+  sendMessage,
 };
 export default httpManager;
