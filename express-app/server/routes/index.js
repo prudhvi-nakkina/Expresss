c9092edd1836858b7e7b32617fe22000e77d2657
Akhil Sai Koppanatham <koppanatham.a@northeastern.edu>
2022-04-22 01:29:06 -0400

    removed login routes from the server section

--- express-app/server/routes/index.js
+++ express-app/server/routes/index.js
@@ -2,15 +2,19 @@
 import * as Controller from "../app/controllers";
 import * as Validation from "../utility/validations";
 const applyRoutes = (app) => {
-  app.get("/", (req, res) => res.send(`API is running fine`));
-  //create-user, login, channel, search-user, channel-list, send-messsage
-  app.post("/user", Validation.validateCreateUser, Controller.createUser);
 
-  app.post("/login", Validation.validateLogin,Controller.loginUser);
-  app.post("/channel",Validation.validateCreateChannel, Controller.createChannel);
-  app.get("/search-user", Validation.validateSearchUser,Controller.searchUser);
-  app.get("/channel-list", Validation.validateGetChannelList,Controller.getChannelList);
-  app.post("/message", Validation.validateAddMessage,Controller.sendMessage);
+  app.get('/', (req, res) => res.send(`API running fine`));
+
+  app.post('/user', Validation.validateCreateUser, Controller.createUser);
+
+  app.get('/search-user', Validation.validateSearchUser, Controller.searchUser);
+
+  app.post('/channel', Validation.validateCreateChannel, Controller.createChannel);
+
+  app.get('/channel-list', Validation.validateGetChannelList, Controller.getChannelList);
+
+  app.post('/message', Validation.validateAddMessage, Controller.sendMessage);
+  
 };
 
 export default applyRoutes;
