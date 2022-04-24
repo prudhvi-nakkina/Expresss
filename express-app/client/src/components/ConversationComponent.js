f32f6ac7f87962167cef25624cc93287ed8b678b
Akhil Sai Koppanatham <koppanatham.a@northeastern.edu>
2022-04-24 19:22:19 -0400

    changed style for some divs and added props for passing some values to the Container with profile header

--- express-app/client/src/components/ConversationComponent.js
+++ express-app/client/src/components/ConversationComponent.js
@@ -1,23 +1,31 @@
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { SearchContainer, SearchInput } from "./ContactListComponent";
-import { messagesList } from "../mockData";
 import Picker from "emoji-picker-react";
-import React, { useState } from "react";
+import { messagesList } from "../mockData";
 import httpManager from "../managers/httpManager";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
   flex: 3;
-  background: white;
+  height: 100%;
+  width: 100%;
+  background: #f6f7f8;
 `;
 
-const Profileheader = styled.div`
+const ProfileHeader = styled.div`
   display: flex;
   flex-direction: row;
   background: #ededed;
   padding: 10px;
+  align-items: center;
+`;
+
+const ProfileInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  background: #ededed;
   align-items: center;
   gap: 10px;
 `;
@@ -27,132 +35,136 @@ const ProfileImage = styled.img`
   height: 32px;
   border-radius: 50%;
 `;
+const ContactName = styled.span`
+  font-size: 16px;
+  color: black;
+`;
 
 const ChatBox = styled.div`
   display: flex;
+  flex-direction: row;
   background: #f0f0f0;
   padding: 10px;
   align-items: center;
   bottom: 0;
 `;
-
-const EmojiImage = styled.img`
-  width: 28px;
-  height: 28px;
-  opacity: 0.4;
-  cursor: pointer;
-`;
-
 const MessageContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
-  background: #e5ddd6;
   overflow-y: auto;
+  background: #e5ddd6;
 `;
-
 const MessageDiv = styled.div`
-  justify-content: ${(props) => (props.isYours ? "flex-end" : "flex-start")};
   display: flex;
+  justify-content: ${(props) => (props.isYours ? "flex-end" : "flex-start")};
   margin: 5px 15px;
 `;
-
 const Message = styled.div`
+  background: ${(props) => (props.isYours ? "#daf8cb" : "white")};
+  padding: 8px 10px;
+  border-radius: 4px;
   max-width: 50%;
   color: #303030;
-  padding: 8px 10px;
   font-size: 14px;
-  border-radius: 4px;
-  background: ${(props) => (props.isYours ? "#daf8cb" : "white")};
 `;
-
-const ConversationComponent = (props) => {
+const EmojiImage = styled.img`
+  width: 28px;
+  height: 28px;
+  border-radius: 50%;
+  opacity: 0.4;
+  cursor: pointer;
+`;
+function ConversationComponent(props) {
   const { selectedChat, userInfo, refreshContactList } = props;
   const [text, setText] = useState("");
   const [pickerVisible, togglePicker] = useState(false);
   const [messageList, setMessageList] = useState([]);
-  const onEmojiClick = (event, emojiObj) => {
-    setText(text + emojiObj.emoji);
-    togglePicker(false);
-  };
-  const onEnterPress = (event) => {
-    let channelId = ""
+
+  useEffect(() => {
+    setMessageList(selectedChat.channelData.messages);
+  }, [selectedChat]);
+
+  const onEnterPress = async (event) => {
+    let channelId = selectedChat.channelData._id;
     if (event.key === "Enter") {
       if (!messageList || !messageList.length) {
-        const channelUsers=[
+        const channelUsers = [
           {
-            email:userInfo.email,
-            name:userInfo.name,
-            profilePic:userInfo.imageUrl,
+            email: userInfo.email,
+            name: userInfo.name,
+            profilePic: userInfo.imageUrl,
           },
           {
-            email:selectedChat.email,
-            name:selectedChat.name,
-            profilePic:selectedChat.profilePic,
+            email: selectedChat.otherUser.email,
+            name: selectedChat.otherUser.name,
+            profilePic: selectedChat.otherUser.profilePic,
           },
-          {
-            email:"",
-            name:"",
-            profilePic:"",
-          }
-        ]
-        const channelResponse = await httpManager.createChannel({channelUsers});
+        ];
+        const channelResponse = await httpManager.createChannel({
+          channelUsers,
+        });
         channelId = channelResponse.data.responseData._id;
-        refreshContactList();
-      } 
+      }
+      refreshContactList();
       const messages = [...messageList];
-    const msgReqData={
-       text,
+      const msgReqData = {
+        text,
         senderEmail: userInfo.email,
         addedOn: new Date().getTime(),
       };
       const messageResponse = await httpManager.sendMessage({
-         channelId,
-         msgReqData 
-      }); 
-      console.log("===", channelId, msgReqData ); 
-      messages.push();
+        channelId,
+        messages: msgReqData,
+      });
+      messages.push(msgReqData);
       setMessageList(messages);
       setText("");
     }
   };
   return (
     <Container>
-      <Profileheader>
-        <ProfileImage src={selectedChat.profilePic} />
-        {selectedChat.name}
-      </Profileheader>
+      <ProfileHeader>
+        <ProfileInfo>
+          <ProfileImage src={selectedChat.otherUser.profilePic} />
+          <ContactName>{selectedChat.otherUser.name}</ContactName>
+        </ProfileInfo>
+      </ProfileHeader>
       <MessageContainer>
-        {messageList.map((messageData) => (
-          <MessageDiv isYours={messageData.senderID === 0}>
-            <Message isYours={messageData.senderID === 0}>
-              {messageData.text}
+        {messageList?.map((messageData) => (
+          <MessageDiv isYours={messageData.senderEmail === userInfo.email}>
+            <Message isYours={messageData.senderEmail === userInfo.email}>
+              {[messageData.text]}
             </Message>
           </MessageDiv>
         ))}
       </MessageContainer>
+
       <ChatBox>
         <SearchContainer>
           {pickerVisible && (
             <Picker
               pickerStyle={{ position: "absolute", bottom: "60px" }}
-              onEmojiClick={onEmojiClick}
+              onEmojiClick={(e, emoji) => {
+                setText(text + emoji.emoji);
+                togglePicker(false);
+              }}
             />
           )}
           <EmojiImage
-            src={"/data.svg"}
-            onClick={() => togglePicker(!pickerVisible)}
-          ></EmojiImage>
+            src={"/whatsapp-clone/data.svg"}
+            onClick={() => togglePicker((pickerVisible) => !pickerVisible)}
+          />
           <SearchInput
-            placeholder="Type a Message"
+            placeholder="Type a message"
             value={text}
             onKeyDown={onEnterPress}
             onChange={(e) => setText(e.target.value)}
-          ></SearchInput>
+          />
         </SearchContainer>
       </ChatBox>
     </Container>
   );
-};
+}
 
 export default ConversationComponent;
