c6a105f6ef47ee51fa91e8809c37ad991f5463c9
Sowri19 <98078922+Sowri19@users.noreply.github.com>
2022-04-27 00:40:49 -0400

    update-src-sass

--- express-app/client/src/index.js
+++ express-app/client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./../dist/main.css";
+import "./dist/main.css";
 import App from "./App";
 import LoginComponent from "./modules/login";
 
